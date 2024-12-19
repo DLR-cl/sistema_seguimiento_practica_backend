805ec2558178a026934e937ca89d69dbaeccdb41
juan yampara <yampara64@gmail.com>
2024-12-19 08:49:07 -0300

    reconocer sql

--- src/modules/academicos/evaluacion-academica/evaluacion-academica.module.ts
+++ src/modules/academicos/evaluacion-academica/evaluacion-academica.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EvaluacionAcademicaController } from './evaluacion-academica.controller';
 import { EvaluacionAcademicaService } from './evaluacion-academica.service';
-import { DatabaseModule } from 'src/database/database/database.module';
+import { DatabaseModule } from '../../../database/database/database.module';
 
 @Module({
   controllers: [EvaluacionAcademicaController],
