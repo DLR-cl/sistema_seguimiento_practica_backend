2751a6269097c9e245c1b8a38ff4b3b19633ecac
Juan Yampara <yampara64@gmail.com>
2024-12-26 08:49:39 -0300

    analiticaService fix

--- src/modules/dashboard/dashboard.module.ts
+++ src/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
 import { DatabaseModule } from 'src/database/database/database.module';
+import { AnaliticaService } from './services/analitica.service';
 
 @Module({
   controllers: [DashboardController],
-  providers: [DashboardService],
+  providers: [DashboardService, AnaliticaService],
   imports: [DatabaseModule]
 })
 export class DashboardModule {}
