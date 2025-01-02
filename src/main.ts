b398bd6f4ab054b886445e94d39ab7381e5b52cd
Juan Yampara <81173069+DLR-cl@users.noreply.github.com>
2025-01-02 10:33:27 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
 app.enableCors({
   origin: (origin, callback) => {
-    const allowedOrigin = 'https://www.diis.cl'; // Dominio único permitido
+    const allowedOrigin = 'https://diis.cl'; // Dominio único permitido
 
     console.log('Origin received:', origin); // Log para depurar el origen recibido
 
