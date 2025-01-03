bf082dfb390c1d9c2432268933e54eeab2a92ede
zBlatzX <fabianorellana67@gmail.com>
2025-01-03 09:15:51 -0300

    fix: comparacion de fecha al extender practica

--- src/modules/practicas/practicas.service.ts
+++ src/modules/practicas/practicas.service.ts
@@ -269,7 +269,7 @@ export class PracticasService {
             throw new BadRequestException(`Error, la práctica con id ${id_practica} no existe`);
         }
 
-        if(fecha_fin_ext < practica.fecha_termino){
+        if(new Date(fecha_fin_ext) <= new Date(practica.fecha_termino)){
             throw new BadRequestException('Error, la fecha de extensión debe ser después de la fecha de termino')
         }
         if(practica.estado == Estado_practica.ESPERA_INFORMES){
