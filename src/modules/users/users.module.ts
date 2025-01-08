368ee272bd7fd743170138aeaed3b8136b139e45
Juan Yampara <yampara64@gmail.com>
2025-01-08 11:20:19 -0300

    fix email avisos import

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 import { DatabaseModule } from '../../database/database/database.module';
+import { EmailAvisosModule } from 'modules/email-avisos/email-avisos.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
-  imports: [DatabaseModule]
+  imports: [DatabaseModule, EmailAvisosModule]
 })
 export class UsersModule {}
